24003452d074825e26b906550e659018e311a630
Pavel Konovalov <scottyrt-r31@mail.ru>
2022-02-16 06:31:43 +0300

    Homework 6.7 to check

--- 05_StaticFinal/02_enum/src/ArithmeticCalculator.java
+++ 05_StaticFinal/02_enum/src/ArithmeticCalculator.java
@@ -3,13 +3,13 @@ public class ArithmeticCalculator {
     public int y;
     public Operation type;
 
-    public ArithmeticCalculator() {
+    public ArithmeticCalculator(int x, int y, Operation type) {
         this.x = x;
         this.y = y;
         this.type = type;
     }
 
-    public void Calculate(int x, int y, Operation type) {
+    public void Calculate() {
         switch (type) {
         case MULTIPLY :
            System.out.println("MULTIPLY result is: " + (x * y));
--- 05_StaticFinal/02_enum/src/Main.java
+++ 05_StaticFinal/02_enum/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        ArithmeticCalculator operation = new ArithmeticCalculator();
-        operation.Calculate(1,2, Operation.MULTIPLY);
+        ArithmeticCalculator operation = new ArithmeticCalculator(1,2, Operation.ADD);
+        operation.Calculate();
     }
 }
