5e23286d1809a51a59dba0d27e836dcd5694faf0
Pavel Konovalov <scottyrt-r31@mail.ru>
2022-02-19 21:35:59 +0300

    Изменен метод calculate, реализована возможность возврата значений

---
+++ 05_StaticFinal/02_enum/src/ArithmeticCalcuiator.java
@@ -0,0 +1,34 @@
+class ArithmeticCalculator {
+    public int x;
+    public int y;
+    public Operation type;
+
+    public ArithmeticCalculator(int x, int y, Operation type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public int getCalculate() {
+        return calculate();
+    }
+
+    public int calculate() {
+//        return switch (type) {
+//            case MULTIPLY -> x * y;
+//            case ADD -> x + y;
+//            case SUBTRACT -> x - y;
+//        };
+
+        if (type == Operation.MULTIPLY) {
+            return x * y;
+        }
+        if (type == Operation.ADD) {
+            return x + y;
+        }
+        if (type == Operation.SUBTRACT) {
+            return x - y;
+        }
+        return 0;
+    }
+}
--- 05_StaticFinal/02_enum/src/Main.java
+++ 05_StaticFinal/02_enum/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         ArithmeticCalculator operation = new ArithmeticCalculator(1,2, Operation.ADD);
-        operation.Calculate();
+        operation.getCalculate();
+        System.out.println(operation.getCalculate());
     }
 }
