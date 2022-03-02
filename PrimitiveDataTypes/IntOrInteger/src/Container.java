e7b340970d444e684a9061d02970445e824858ba
Pavel Konovalov <scottyrt-r31@mail.ru>
2022-03-02 06:11:25 +0300

    Practice work 7.9 to check

--- PrimitiveDataTypes/IntOrInteger/src/Container.java
+++ PrimitiveDataTypes/IntOrInteger/src/Container.java
@@ -1,19 +1,12 @@
 public class Container {
-<<<<<<< HEAD
+
     private Integer count = 0;
-=======
-    private Integer count;
->>>>>>> 03b9394dc52757571fb9d141972d9f4223ff2a15
 
     public void addCount(int value) {
         count = count + value;
     }
 
-<<<<<<< HEAD
     public Integer getCount() {
-=======
-    public int getCount() {
->>>>>>> 03b9394dc52757571fb9d141972d9f4223ff2a15
-        return count;
+            return count;
+        }
     }
-}
